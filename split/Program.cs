fa405e21c182a2f093244262f9b8d0eb1a085332
Михаил Денисов <migelius.invesy@gmail.com>
2022-04-16 22:03:32 +0300

    Добавьте файлы проекта.

---
+++ split/Program.cs
@@ -0,0 +1,15 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        string text = "Чем больше женщину мы любим, тем больше меньше мы чем тем. В.Кличко";
+        string[] arrayText = text.Split(' ');
+
+        Console.WriteLine(text);
+
+        foreach (string word in arrayText)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
